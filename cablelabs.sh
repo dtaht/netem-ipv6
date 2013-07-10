32ead82c620c785c0dc1e663d4132ddf983d8daf
Dave Täht <dave.taht@bufferbloat.net>
2013-07-10 00:08:03 -0700

    First semi-working version
    I am not happy with the idea of using the prio qdisc here.

--- cablelabs.sh
+++ cablelabs.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 # Configure netem so that link local packets aren't shaped
 # and run at line rate (well, with fq_codel)
+# One of the things that http://www.lartc.org/lartc.html
+# makes very unclear is that they are matching against
+# 6in4 packets, not ipv6 packets. This matches against
+# native ipv6.
 
-IFACE=eth2
 . ./test_setup.inc
 
-ethtool -K $IFACE tso off gso off gro off
-TC=`which tc`
+TC=
 
+# My problem with using prio for this is that its 
+# dependent on the granuality of timestamping in netem
+# DRR or (better) SFQ makes more sense but what we
+# really want is a FIFO in, spread out, and whatever
+# packet is the "Readiest to be delivered", delivered
+
+delay() {
+IFACE=$1
 tc qdisc del dev $IFACE root 2>/dev/null
-#tc -batch <<EOF
+tc -batch <<EOF
 $TC qdisc add dev $IFACE root handle 1: prio bands 10
 $TC qdisc add dev $IFACE parent 1:1 handle 11: $N $S1_ARGS
 $TC qdisc add dev $IFACE parent 1:2 handle 12: $N $S2_ARGS
@@ -26,8 +36,7 @@ $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 5 u32 match u16 0xfe80 0
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 6 u32 match u16 0xff02 0xffff at 24 flowid 1:a
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 7 u32 match u16 0xfe80 0xffff at 24 flowid 1:a
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 8 u32 match u16 0xff02 0xffff at 8 flowid 1:a
-
-$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 101 u32 $MS match u8 0x0a 0xff at 23 flowid 1:1
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 101 u32 $MS match u8 0x01 0xff at 23 flowid 1:1
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 102 u32 $MS match u8 0x02 0xff at 23 flowid 1:2
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 103 u32 $MS match u8 0x03 0xff at 23 flowid 1:3
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 104 u32 $MS match u8 0x04 0xff at 23 flowid 1:4
@@ -36,8 +45,43 @@ $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 106 u32 $MS match u8 0x0
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 107 u32 $MS match u8 0x07 0xff at 23 flowid 1:7
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 108 u32 $MS match u8 0x08 0xff at 23 flowid 1:8
 $TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 109 u32 $MS match u8 0x09 0xff at 23 flowid 1:9
-$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 109 u32 $MS match u8 0x0a 0xff at 23 flowid 1:a
-#EOF
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 110 u32 $MS match u8 0x0a 0xff at 23 flowid 1:a
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 201 u32 $MD match u8 0x01 0xff at 39 flowid 1:1
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 202 u32 $MD match u8 0x02 0xff at 39 flowid 1:2
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 203 u32 $MD match u8 0x03 0xff at 39 flowid 1:3
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 204 u32 $MD match u8 0x04 0xff at 39 flowid 1:4
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 205 u32 $MD match u8 0x05 0xff at 39 flowid 1:5
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 206 u32 $MD match u8 0x06 0xff at 39 flowid 1:6
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 207 u32 $MD match u8 0x07 0xff at 39 flowid 1:7
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 208 u32 $MD match u8 0x08 0xff at 39 flowid 1:8
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 209 u32 $MD match u8 0x09 0xff at 39 flowid 1:9
+$TC filter add dev $IFACE protocol ipv6 parent 1:0 prio 210 u32 $MD match u8 0x0a 0xff at 39 flowid 1:a
+EOF
+}
+
+setup() {
+IFACE=$1
+ethtool -K $IFACE tso off gso off gro off
+$INSMOD ifb
+$INSMOD act_mirred
+$INSMOD sch_netem
+}
+
+redir() {
+IFACE=$1
+DEV=$2
+tc qdisc del dev $IFACE handle ffff: ingress 2> /dev/null
+tc qdisc add dev $IFACE handle ffff: ingress
+ifconfig $DEV up
+tc filter add dev $IFACE parent ffff: protocol all prio 10 u32 \
+   match u32 0 0 action mirred egress redirect dev $DEV
+}
+
+case $1 in
+	up) setup $2; delay $2; delay ifb0; redir $2 ifb0;;
+	down) tc qdisc del dev $2 ingress; tc qdisc del dev $2 root; tc qdisc del dev ifb0 root;;
+	*) echo "$0 up/down interface" "args incorrect $*"
+esac
 
 exit
 
--- server_setup.sh
+++ server_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # Setup a server
 
-IFACE=lo
-
 . ./test_setup.inc
 
 updn() {
-arg=$1
+IFACE=$1
+arg=$2
+
 ip -6 addr $arg $S1/128 dev $IFACE
 ip -6 addr $arg $S2/128 dev $IFACE
 ip -6 addr $arg $S3/128 dev $IFACE
@@ -18,9 +18,22 @@ ip -6 addr $arg $S8/128 dev $IFACE
 ip -6 addr $arg $S9/128 dev $IFACE
 }
 
+gen_hosts() {
+echo $S1 `echo $S1_ARGS | sed 's/ /-/g'`
+echo $S2 `echo $S2_ARGS | sed 's/ /-/g'`
+echo $S3 `echo $S3_ARGS | sed 's/ /-/g'`
+echo $S4 `echo $S4_ARGS | sed 's/ /-/g'`
+echo $S5 `echo $S5_ARGS | sed 's/ /-/g'`
+echo $S6 `echo $S6_ARGS | sed 's/ /-/g'`
+echo $S7 `echo $S7_ARGS | sed 's/ /-/g'`
+echo $S8 `echo $S8_ARGS | sed 's/ /-/g'`
+echo $S9 `echo $S9_ARGS | sed 's/ /-/g'`
+}
+
 case $1 in
 	up) updn add ;;
 	down) updn del ;;
 	restart) updn del; updn up;;
-	*) $0 up or down
+	hosts) gen_hosts;;
+	*) echo $0 up or down
 esac
